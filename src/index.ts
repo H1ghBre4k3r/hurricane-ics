50c8141d74308cb7fcc4176ec82ad5ad920032cd
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-06-12 01:21:53 +0200

    Add sample ICS config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-import express from "express";
+import http from "http";
+import ical from "ical-generator";
 
-const router = express();
-
-router.get("/", (_, res) => {
-    res.send("Hello, world!");
+const calendar = ical({ name: "Hurricane Calendar" });
+const startTime = new Date();
+const endTime = new Date();
+endTime.setHours(startTime.getHours() + 1);
+calendar.createEvent({
+    start: startTime,
+    end: endTime,
+    summary: "Example Event",
+    description: "It works ;)",
+    location: "my room",
+    url: "http://sebbo.net/",
 });
 
-router.listen(3000, () => {
-    console.log("Started listening...");
+http.createServer((_, res) => calendar.serve(res)).listen(3000, "127.0.0.1", () => {
+    console.log("Server running at http://127.0.0.1:3000/");
 });
