f95303fb7440ba93bb024ba55f9c50e1101bd12a
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-06-12 01:12:36 +0200

    Initial router setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import express from "express";
+
+const router = express();
+
+router.get("/", (_, res) => {
+    res.send("Hello, world!");
+});
+
+router.listen(3000, () => {
+    console.log("Started listening...");
+});
