316e4916968a80c72c4d624c1cd6c933fe154fd1
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-06-12 01:40:13 +0200

    Add first event

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,29 @@
 import http from "http";
-import ical from "ical-generator";
+import ical, { ICalEventData } from "ical-generator";
 
 const calendar = ical({ name: "Hurricane Calendar" });
-const startTime = new Date();
-const endTime = new Date();
-endTime.setHours(startTime.getHours() + 1);
-calendar.createEvent({
-    start: startTime,
-    end: endTime,
-    summary: "Example Event",
-    description: "It works ;)",
-    location: "my room",
-    url: "http://sebbo.net/",
-});
+
+enum Day {
+    thursday = 0,
+    friday = 1,
+    saturday = 2,
+    sunday = 3,
+}
+
+function event(day: Day, hour: number): Date {
+    return new Date(2022, 5, 16 + day, hour);
+}
+
+const concerts: ICalEventData[] = [
+    {
+        summary: "ALEX MOFA GANG",
+        location: "Wild Coast Stage",
+        start: event(Day.thursday, 20),
+        end: event(Day.thursday, 21),
+    },
+];
+
+calendar.events(concerts);
 
 http.createServer((_, res) => calendar.serve(res)).listen(3000, "127.0.0.1", () => {
     console.log("Server running at http://127.0.0.1:3000/");
