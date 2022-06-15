c9fde33cafde14081e316083bea6c49a1974aef2
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-06-15 21:09:39 +0200

    Adjust size of cards

--- frontend/src/Artist.tsx
+++ frontend/src/Artist.tsx
@@ -14,9 +14,9 @@ export const Artist: FC<ArtistProps> = ({ show, selected, setSelected }) => {
     return (
         <div
             onClick={() => setSelected(!selected)}
-            className={`inline-block bg-white dark:bg-slate-800 rounded-lg px-6 py-6 ring-1 m-2 w-fit ring-slate-900/5 shadow-xl ${
+            className={`inline-block bg-white dark:bg-slate-800 rounded-lg px-2 py-2 ring-1 m-2 w-fit ring-slate-900/5 shadow-xl ${
                 selected ? "border-blue-400" : "border-white dark:border-slate-800"
-            } border-8 `}
+            } border-4 `}
         >
             <h3 className="text-slate-900 dark:text-white text-base font-medium tracking-tight">{name}</h3>
             <p className="text-slate-500 dark:text-slate-400  text-sm">{start.toLocaleString()}</p>
