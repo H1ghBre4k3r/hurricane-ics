af88329845b95996ddd06fa3556d984c51e53aef
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2022-06-15 18:08:34 +0200

    Fix bug in cache

--- src/festival.ts
+++ src/festival.ts
@@ -1,20 +1,25 @@
 import fetch from "node-fetch";
 import { FestivalPlan } from "./types";
 
+function diff_minutes(dt2: Date, dt1: Date) {
+    var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+    diff /= 60;
+    return Math.abs(Math.round(diff));
+}
+
 /**
  * Fetch the festival plan by scraping the lineup website.
  */
 export const fetchFestivalFactory = () => {
-    let last_fetch: Date | null = null;
+    let last_fetch: Date = new Date();
 
     let cache: FestivalPlan | null = null;
 
     return async (): Promise<FestivalPlan> => {
         const now = new Date();
-        var diffMs = now.getTime() - (last_fetch?.getTime() ?? 0);
-        const diff = Math.round(((diffMs % 86400000) % 3600000) / 60000);
+        const diff = diff_minutes(now, last_fetch);
 
-        if (diff > 15) {
+        if (diff > 15 || cache === null) {
             const response = await fetch("https://hurricane.de/de/line-up");
             const raw = await response.text();
             const match = /var\s+timetableEvents\s*=\s*(\{.+?\});/.exec(raw);
